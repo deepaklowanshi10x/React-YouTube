0e26b8ecc7a4521063149e3b49d10fde291496e1
DEEPAK LOWANSHI <130597528+deepaklowanshi@users.noreply.github.com>
2026-05-24 14:47:27 +0530

    final Counter Project

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -4,20 +4,23 @@ import { useState } from 'react'
 // import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
+function App() { 
 
-  let[counter, setCounter] = useState(10)
+  let[counter, setCounter] = useState(1)
   const addValue = () => {
-  setCounter (counter + 1);
-} 
-
-const removeValue= () => {
+    if(counter < 20){
+      setCounter (counter + 1)
+    }
+  }
+const removeValue= () => { 
+if(counter > 0){
   setCounter (counter - 1);
-} 
+}  
+}
 
   return (
     <>
-     <h1>JavaScript YouTube</h1>
+     <h1>React Counter Project</h1>
      <h2>Counter value: {counter}</h2>
 
      <button
